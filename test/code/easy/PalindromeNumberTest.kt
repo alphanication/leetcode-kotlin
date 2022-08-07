cb3a426e64afcb505ff59687bb5730db7fabd2d6
vorobeye <russianfederation.vorobey@gmail.com>
2022-08-07 16:28:03 +0300

    PalindromeNumber

---
+++ test/code/easy/PalindromeNumberTest.kt
@@ -0,0 +1,32 @@
+package code.easy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class PalindromeNumberTest {
+
+    @Test
+    fun isPalindrome1() {
+        val inputNumber = 121
+        assertTrue(PalindromeNumber().isPalindrome(x = inputNumber))
+    }
+
+    @Test
+    fun isPalindrome2() {
+        val inputNumber = -121
+        assertFalse(PalindromeNumber().isPalindrome(x = inputNumber))
+    }
+
+    @Test
+    fun isPalindrome3() {
+        val inputNumber = 10
+        assertFalse(PalindromeNumber().isPalindrome(x = inputNumber))
+    }
+
+    @Test
+    fun isPalindrome4() {
+        val inputNumber = 242
+        assertTrue(PalindromeNumber().isPalindrome(x = inputNumber))
+    }
+}
