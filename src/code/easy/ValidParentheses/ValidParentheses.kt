2dcfd80734fd3dc68e6f49432d254b194a1480fb
vorobeye <russianfederation.vorobey@gmail.com>
2022-08-12 08:32:53 +0300

    add ValidParentheses

---
+++ src/code/easy/ValidParentheses/ValidParentheses.kt
@@ -0,0 +1,33 @@
+package code.easy.ValidParentheses
+
+import java.util.*
+import kotlin.collections.HashMap
+
+// TODO: Vorobey Egor / 12.08.2022
+
+class ValidParentheses {
+
+    fun isValid(s: String): Boolean {
+        val hashMapParentheses = hashMapEqualsParentheses()
+        var stack = Stack<String>()
+        s.forEach {
+            when (it.toString()) {
+                "{" -> stack.push(hashMapParentheses[it].toString())
+                "(" -> stack.push(hashMapParentheses[it].toString())
+                "[" -> stack.push(hashMapParentheses[it].toString())
+                else -> if (stack.isEmpty() || stack.pop() != it.toString()) return false
+            }
+        }
+
+        return stack.isEmpty()
+    }
+
+    fun hashMapEqualsParentheses(): HashMap<Char, Char> {
+        val hashMapParentheses = HashMap<Char, Char>()
+        hashMapParentheses['('] = ')'
+        hashMapParentheses['{'] = '}'
+        hashMapParentheses['['] = ']'
+
+        return hashMapParentheses
+    }
+}
---
+++ test/code/easy/ValidParentheses/ValidParenthesesTest.kt
@@ -0,0 +1,45 @@
+package code.easy.ValidParentheses
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ValidParenthesesTest {
+
+    @Test
+    fun isValid1() {
+        val stringIsValid = "()"
+        assertTrue(ValidParentheses().isValid(s = stringIsValid))
+    }
+
+    @Test
+    fun isValid2() {
+        val stringIsValid = "()[]{}"
+        assertTrue(ValidParentheses().isValid(s = stringIsValid))
+    }
+
+    @Test
+    fun isValid3() {
+        val stringIsNotValid = "(]"
+        assertFalse(ValidParentheses().isValid(s = stringIsNotValid))
+    }
+
+    @Test
+    fun isValid4() {
+        val stringIsNotValid = "([)]"
+        assertFalse(ValidParentheses().isValid(s = stringIsNotValid))
+    }
+
+    @Test
+    fun isValid5() {
+        val stringIsNotValid = "{[]}"
+        assertTrue(ValidParentheses().isValid(s = stringIsNotValid))
+    }
+
+    @Test
+    fun hashMapEqualsParentheses() {
+        assertEquals(')', ValidParentheses().hashMapEqualsParentheses()['('])
+        assertEquals('}', ValidParentheses().hashMapEqualsParentheses()['{'])
+        assertEquals(']', ValidParentheses().hashMapEqualsParentheses()['['])
+    }
+}
