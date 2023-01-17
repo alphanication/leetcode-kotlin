085b999cc75d90367ce08544d828d2b0459c8669
vorobeye <russianfederation.vorobey@gmail.com>
2023-01-18 00:32:04 +0300

    single number - solution

---
+++ src/code/easy/SingleNumber/SingleNumber.kt
@@ -0,0 +1,8 @@
+package code.easy.SingleNumber
+
+private const val DUPLICATED_NUMBER = 1
+
+class SingleNumber {
+
+    fun singleNumber(nums: IntArray) : Int = nums.first { x -> nums.count { it == x } == DUPLICATED_NUMBER }
+}
