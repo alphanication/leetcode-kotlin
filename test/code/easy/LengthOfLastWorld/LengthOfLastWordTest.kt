800c4c3ef235ae6d18e9a5dffcf1e242f183e822
vorobeye <russianfederation.vorobey@gmail.com>
2023-01-17 23:46:19 +0300

    length of last word - tests

---
+++ test/code/easy/LengthOfLastWorld/LengthOfLastWordTest.kt
@@ -0,0 +1,31 @@
+package code.easy.LengthOfLastWorld
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LengthOfLastWordTest {
+
+    @Test
+    fun lengthOfLastWord1() {
+        val inputStr = "   fly me  "
+        val expectedLengthLastWord = 2
+
+        assertEquals(expectedLengthLastWord, LengthOfLastWord().lengthOfLastWord(inputStr))
+    }
+
+    @Test
+    fun lengthOfLastWord2() {
+        val inputStr = "per aspera ad astra"
+        val expectedLengthLastWord = 5
+
+        assertEquals(expectedLengthLastWord, LengthOfLastWord().lengthOfLastWord(inputStr))
+    }
+
+    @Test
+    fun lengthOfLastWord3() {
+        val inputStr = "simplest way"
+        val expectedLengthLastWord = 3
+
+        assertEquals(expectedLengthLastWord, LengthOfLastWord().lengthOfLastWord(inputStr))
+    }
+}
